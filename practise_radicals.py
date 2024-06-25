1ddab8e1369b16a3aaf4f8f29e8a18f47a1107f5
Kris <kristian_dilov@abv.bg>
2024-06-25 07:43:50 +0100

    add more radicals

--- practise_radicals.py
+++ practise_radicals.py
@@ -1,12 +1,34 @@
 import random
 
 
-radicals = {"本": "book", "弓": "bow", "子": "child"}
+radicals = {
+    "本": "book",
+    "弓": "bow",
+    "子": "child",
+    "牛": "cow",
+    "彡": "hair",
+    "手": "hand",
+    "冂": "head",
+    "天": "heaven",
+    "王": "king",
+    "儿": "legs",
+    "中": "middle",
+    "月": "moon",
+    "ナ": "narwhal",
+    "ム": "private",
+    "田": "rice paddy",
+    "小": "small",
+    "立": "stand",
+}
 
+
+kanji_readings = {"大": "tie", "正": "sho"}
+
+kanji_meaning = {"正": "correct"}
 
 
 if __name__ == "__main__":
-    
+
     score = 0
     total_kanji_shown = 0
     while True:
@@ -14,21 +36,13 @@ if __name__ == "__main__":
 
         radical, mnemonic = random.choice(list(radicals.items()))
         print(radical)
-        
+
         radical_meaning = input("enter radical meaning: ")
 
         if radical_meaning != mnemonic:
-            print("\nWRONG, TRY AGAIN!")
+            print(f"\nWRONG, ITS: {mnemonic}!")
         else:
             print("\nCORRECT")
             score += 1
 
         print(f"Correct {score} out of {total_kanji_shown}\n")
-
-
-
-
-
-
-
-
